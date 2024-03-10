f29e1f5d8a4b06c5e5f1cddbfcb1d9e98bd5a400
pavelkurtukov <pavel@kurtukov.ru>
2024-03-10 14:08:12 +0700

    added missed mapping annotation

--- src/main/java/ru/kurtukov/spingbootdemo/controller/ProfileController.java
+++ src/main/java/ru/kurtukov/spingbootdemo/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package ru.kurtukov.spingbootdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kurtukov.spingbootdemo.profile.SystemProfile;
@@ -13,6 +14,7 @@ public class ProfileController {
         this.profile = profile;
     }
 
+    @GetMapping("profile")
     public String getProfile() {
         return profile.getProfile();
     }

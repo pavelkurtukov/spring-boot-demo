22fbb57400f6f83754330c6dbf2bbed62bb08b0e
pavelkurtukov <pavel@kurtukov.ru>
2024-03-10 13:38:51 +0700

    Added profile choosing annotation

--- src/main/java/ru/kurtukov/spingbootdemo/config/JavaConfig.java
+++ src/main/java/ru/kurtukov/spingbootdemo/config/JavaConfig.java
@@ -1,5 +1,6 @@
 package ru.kurtukov.spingbootdemo.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import ru.kurtukov.spingbootdemo.profile.DevProfile;
 import ru.kurtukov.spingbootdemo.profile.SystemProfile;
@@ -7,11 +8,13 @@ import ru.kurtukov.spingbootdemo.profile.ProductionProfile;
 
 public class JavaConfig {
     @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }

06c07f28899f1e08d8f4b3c76b85dd3a24b4ff84
pavelkurtukov <pavel@kurtukov.ru>
2024-03-10 13:25:14 +0700

    Initial commit

---
+++ src/main/java/ru/kurtukov/spingbootdemo/SpingBootDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.kurtukov.spingbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kurtukov/spingbootdemo/SpingBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kurtukov.spingbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

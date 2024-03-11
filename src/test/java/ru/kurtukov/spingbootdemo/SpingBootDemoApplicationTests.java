159e0a7c4b96407a2c9b1f75ce692b1c4e47659b
pavelkurtukov <pavel@kurtukov.ru>
2024-03-12 03:24:40 +0700

    Интеграционное тестирование

--- src/test/java/ru/kurtukov/spingbootdemo/SpingBootDemoApplicationTests.java
+++ src/test/java/ru/kurtukov/spingbootdemo/SpingBootDemoApplicationTests.java
@@ -1,13 +1,36 @@
 package ru.kurtukov.spingbootdemo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpingBootDemoApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	private static final GenericContainer<?> devApp = new GenericContainer<>("devapp").withExposedPorts(8080);
+	private static final GenericContainer<?> prodApp = new GenericContainer<>("prodapp").withExposedPorts(8081);
+
+	@BeforeAll
+	public static void setUp() {
+		devApp.start();
+		prodApp.start();
+	}
 
 	@Test
-	void contextLoads() {
+	void testDevAppPort() {
+		String expected = "Current profile is dev";
+
+		ResponseEntity<String> entityDevApp = restTemplate.getForEntity("http://localhost:" + devApp.getMappedPort(8080) + "/profile", String.class);
+		String result = entityDevApp.getBody();
+
+		Assertions.assertEquals(expected, result);
 	}
 
 }
